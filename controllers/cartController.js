c1bc2f2e799994b0cb34ce8d1e5aa9edd0918f48
praakash7542-jp <jyotimaurya7542@gmail.com>
2026-07-12 17:13:17 +0530

    removeItemFromCart,clearCart

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -109,4 +109,56 @@ const updateQuantity  = async (req,res)=>{
     }
 }
 
-module.exports = { addToCart,viewCart,updateQuantity };
+const removeItemFromCart = async (req, res) => {
+  try {
+    const userId = req.user.id;
+    const { productId } = req.params; // URL se product id uthayenge
+
+    const cart = await Cart.findOne({ user: userId });
+    if (!cart) {
+      return res.status(404).json({ message: 'Cart not found' });
+    }
+
+    cart.items = cart.items.filter(item => item.product.toString() !== productId);
+    await cart.save();
+
+    const updatedCart = await cart.populate({
+      path: 'items.product',
+      select: 'name price images stock'
+    });
+
+    res.status(200).json({
+      success: true,
+      message: 'Item removed from cart',
+      data: updatedCart
+    });
+  } catch (error) {
+    res.status(500).json({ message: 'Server Error', error: error.message });
+  }
+};
+
+
+const clearCart = async (req, res) => {
+  try {
+    const userId = req.user.id;
+
+    const cart = await Cart.findOne({ user: userId });
+    if (!cart) {
+      return res.status(404).json({ message: 'Cart not found' });
+    }
+
+    cart.items = [];
+    await cart.save();
+
+    res.status(200).json({
+      success: true,
+      message: 'Cart cleared successfully',
+      data: cart
+    });
+  } catch (error) {
+    res.status(500).json({ message: 'Server Error', error: error.message });
+  }
+};
+
+
+module.exports = { addToCart,viewCart,updateQuantity ,removeItemFromCart,clearCart};
--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -1,6 +1,6 @@
 const express =  require('express');
 const router = express.Router();
-const {addToCart,viewCart,updateQuantity} = require('../controllers/cartController');
+const {addToCart,viewCart,updateQuantity,removeItemFromCart,clearCart} = require('../controllers/cartController');
 const {protect} = require('../middleware/authMiddleware');
 const { route } = require('./authRoutes');
 
@@ -8,4 +8,6 @@ const { route } = require('./authRoutes');
 router.post('/',protect ,addToCart);
 router.get('/',protect,viewCart);
 router.put('/',protect,updateQuantity);
+router.delete('/item/:productId',protect,removeItemFromCart);
+router.delete('/',protect,clearCart);
 module.exports = router;
